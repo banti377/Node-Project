9ffeb797fe29c0b5098e9ee338f3842b4b7e6908
Banti Patel <bantipatel2506@gmail.com>
2024-07-31 14:33:06 +0530

    add data

--- src/router/user.js
+++ src/router/user.js
@@ -7,6 +7,24 @@ import {
   sendOTP,
 } from "../controller/user";
 import { auth } from "../middleWare/auth";
+import multer from "multer";
+import fs from "fs";
+import { imageUpload } from "../middleWare/imageUpload";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = "/public/";
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, "profile" + "." + `${file.originalname.split(".").pop()}`);
+  },
+});
+
+const upload = multer({ storage });
 
 const router = express.Router();
 
@@ -19,5 +37,10 @@ router.post("/forget-password", forgetPassword);
 router.post("/reset-password", auth, resetPassword);
 
 router.post("/sendOtp", sendOTP);
+
+router.post("/uploadImg", upload.single("avatar"), async (req, res) => {
+  let url = await imageUpload(req.file.url);
+  res.status(200).send({ data: url, success: true, message: "" });
+});
 
 export default router;
